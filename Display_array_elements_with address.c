fea518a5b9f799f4bf7ff536b4df8c9f9e2d50a1
Sumit-Sharma001 <11sumitkumaryt@gmail.com>
2025-12-25 11:15:13 +0530

    Add files via upload

---
+++ Display_array_elements_with address.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int rows,col;
+    printf("enter the number of rows",rows);
+    scanf("%d",&rows);
+    printf("enter the number of columns",col);
+    scanf("%d",&col);
+    int mat[i][j];
+    for(int i=1;i<=rows;i++)
+    {
+        for(int j=1;j<=col;j++)
+        {
+        printf("enter the elements");
+    scanf("%d",mat[i][j]);
+    }
+}
+}
