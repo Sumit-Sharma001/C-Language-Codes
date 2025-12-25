815eb99a3dd9172ac4855e22c08afaf7aab94169
Sumit-Sharma001 <11sumitkumaryt@gmail.com>
2025-12-25 11:12:42 +0530

    Add files via upload

---
+++ To_check_the_whether_the_number_is_possitive _negative,_or_zero.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    int area;
+    printf("enter the number",a);
+    scanf("%d",&a);
+    if (a>=0)
+    {
+      if(a==0)
+      {
+      printf("the number is zero");
+      }
+      else
+      printf("the number is possitive");
+    }
+      if(a<0)
+      {
+        printf("the number is negative");
+      } 
+     
+    
+}
